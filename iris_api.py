20b4dcf041abf358c2287e5b7121264e0b06c444
Kannan <65953360+kannan-dedsec@users.noreply.github.com>
2025-11-12 21:40:16 +0530

    Update iris_api.py

--- iris_api.py
+++ iris_api.py
@@ -1,15 +1,26 @@
 from fastapi import FastAPI
 import joblib
+from pydantic import BaseModel
 import numpy as np
 
+class InputVector(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
 
 app = FastAPI()
 
+MODEL_PATH = "model/model.joblib"
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome its working !! "}
 
 @app.post("/predict")
-def predict():
-    return {"prediction": "setosa"}
+def predict(input: InputVector):
+    model = joblib.load(MODEL_PATH)
+    data = np.array([[input.sepal_length, input.sepal_width,
+                      input.petal_length, input.petal_width]])
+    preds = model.predict(data)
+    return {"prediction": preds.tolist()}
